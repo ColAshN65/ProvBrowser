244136348920dfc304eb61ad75afd5f4c95ceb60
shurutovas <lagershurutov@gmail.com>
2025-03-26 17:33:47 +0700

    Пофикшен текстбокс в строке поиска

--- ProvBrowser/View/Components/BrowserItemComponent.xaml.cs
+++ ProvBrowser/View/Components/BrowserItemComponent.xaml.cs
@@ -24,5 +24,17 @@ namespace ProvBrowser.View.Components
         {
             InitializeComponent();
         }
+
+        private void TextBox_KeyEnterUpdate(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                TextBox tBox = (TextBox)sender;
+                DependencyProperty prop = TextBox.TextProperty;
+
+                BindingExpression binding = BindingOperations.GetBindingExpression(tBox, prop);
+                if (binding != null) { binding.UpdateSource(); }
+            }
+        }
     }
 }
